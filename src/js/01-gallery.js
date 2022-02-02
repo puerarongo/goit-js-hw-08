721da31edca0b4284ccde03ae8b4808082a69ac7
puerarongo <akhardin96@gmail.com>
2022-02-02 21:52:29 +0300

    complete exercise number 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,5 @@
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -10,58 +11,23 @@ const galleryEl = document.querySelector(".gallery");
 // * Stage 1.1
 
 const galleryInnerEl = galleryItems.map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-  <a class="gallery__link" href=${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
-    
+    return `<li><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a></li>`  
 }).join("");
 
 // * Stage 1.2
 
 galleryEl.insertAdjacentHTML("beforeend", galleryInnerEl);
 
+const a = galleryEl.querySelectorAll(".gallery__link")
+console.log(a)
+
+
 // todo Stage 2
 
-
-galleryEl.addEventListener("click", selectPicture);
-
-
-const instance = basicLightbox.create(`<img class="picture__lightbox">`,
-  {
-    onShow: (condition) => {
-          window.addEventListener("keydown", keyboardEvent);
-    }
-  },
-  {
-    onClose: (condition) => {
-          window.removeEventListener("keydown", keyboardEvent);
-    }
-  }
-);
-
-
-function selectPicture(event) {
-    event.preventDefault();
-
-  if (event.target.nodeName === "IMG") {
-    instance.element().querySelector(".picture__lightbox")
-      .setAttribute("src", `${event.target.dataset.source}`)
-
-      instance.show();
-  };
-}; 
-
-function keyboardEvent(event) {
-  if (event.key === "Escape") {
-    instance.close()
-    console.log("!")
-  }
-}
-
+   const lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: 'alt',
+      captionDelay: 250,
+      captionPosition: 'bottom',
+    });
