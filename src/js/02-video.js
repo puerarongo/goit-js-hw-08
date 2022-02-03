6dcd6d46d4a999bc3cd83441021b00daba092246
puerarongo <akhardin96@gmail.com>
2022-02-03 19:17:03 +0300

    Add exercise 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const PLAY_KEY = "videoplayer-current-time";
+const iframeEl = document.querySelector("iframe");
+const playerEl = new Player(iframeEl);
+
+
+const onPlay = () => {
+    playerEl.on("timeupdate", throttle(timeEvent, 1000)); 
+};
+
+const timeEvent = (event) => {
+    console.log(event.seconds);
+    localStorage.setItem(PLAY_KEY, event.seconds);
+};
+
+playerEl.on("play", onPlay);
+playerEl.setCurrentTime(localStorage.getItem(PLAY_KEY));
