f08afe4d6ee649215e58f9f9127c82571136d006
puerarongo <akhardin96@gmail.com>
2022-02-09 16:06:56 +0300

    exercise 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
 
+// ! Stage 1
+
+// ? HTML tegs
+const formEl = document.querySelector(".feedback-form");
+const emailEl = formEl.querySelector("input");
+const textEl = formEl.querySelector("textarea");
+const submitEl = formEl.querySelector("button");
+
+// *
+const FORM_KEY = "feedback-form-state";
+const getForm = localStorage.getItem(FORM_KEY);
+let formValues = {}
+
+
+const formValidation = (event) => {
+    const into = event.target
+    if (into.name === "email") {
+        formValues.email = into.value
+    }
+
+    else if (into.name === "message") {
+        formValues.message = into.value
+    }
+
+    localStorage.setItem(FORM_KEY, JSON.stringify(formValues)); 
+};
+
+const parseValues = JSON.parse(getForm);
+
+
+if (getForm) {
+    emailEl.value = parseValues.email
+    textEl.value = parseValues.message
+};
+
+
+const buttonAction = (event) => {
+    event.preventDefault();
+
+    console.log("!")
+}
+
+
+formEl.addEventListener("input", throttle(formValidation, 500));
+submitEl.addEventListener("submit", buttonAction);
